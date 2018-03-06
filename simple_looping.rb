0d8e36924a0b760f57d399d16bd8134d185814ee
Celeste Gerard <celeste.gerard@gmail.com>
2018-03-06 14:11:29 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,8 +4,7 @@ def loop_iterator(number_of_times)
   loop do 
     counter += 1
     puts "Welcome to Flatiron School's Web Development Course!"
-    if number_of_times == counter
-      break
+      break if number_of_times == counter
     end
 end
 
